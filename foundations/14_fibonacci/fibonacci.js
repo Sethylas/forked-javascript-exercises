1c6c4c786dbd6148e17009c590649b5a9fe66d31
Sethylas <60220827+Sethylas@users.noreply.github.com>
2026-06-28 14:54:45 +0800

    Completed tasks

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg);
+    }
+    else {
+        count = countArg;
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    const fib = [0, 1];
+
+    return firstPrev;
 };
 
 // Do not edit below this line
